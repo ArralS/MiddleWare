4b8fd7c741800b9fbda4cc68ee4e5cebfb30b879
ArralS <endiq@DESKTOP-KCUC89O>
2021-04-24 15:40:25 +0200

    Dodaj pliki projektów.

---
+++ MW1.0/Models/MiddleWare.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MW1._0.Models
+{
+    public class MiddleWare
+    {
+      
+        private RequestDelegate _next;
+        public MiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var x = context.Request.Headers["User-Agent"][0].ToLower();
+
+            if (x.Contains("edge") || x.Contains("edg/") || x.Contains("trident"))
+            {
+
+                return context.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+
+            }
+            else return _next(context);
+
+        }
+    }
+}
